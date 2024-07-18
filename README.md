d9ec91bdc5975418fdf67a39ec259fb6242da1b0
Anshita Jaiswal <ajais740@gmail.com>
2024-07-19 00:35:36 +0530

    add baisc redux setup

--- README.md
+++ README.md
@@ -22,3 +22,5 @@ index.css
 
 dependencies
 npm i @reduxjs/toolkit react-redux react-router-dom react-icons react-chartjs-2 chart.js daisyui axios react-hot-toast @tailwindcss/line-clamp
+
+npm i eslint-plugin-simple-import-sort
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-
-
+import {Routes} from 'react-router-dom'
+const App = () => {
   return (
-    <>
-     lms
-    </>
+    <div>
+      lmb
+    </div>
   )
 }
 
 export default App
+
